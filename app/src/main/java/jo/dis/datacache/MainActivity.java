d3851f7ac11c54aad2f0e227ecf9690ab381ed11
Dis <dis123520@gmail.com>
2015-12-04 10:21:33 +0800

    Update

--- app/src/main/java/jo/dis/datacache/MainActivity.java
+++ app/src/main/java/jo/dis/datacache/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView textView1 = (TextView) findViewById(R.id.tv);
         TextView textView2 = (TextView) findViewById(R.id.tv1);
         TextView textView3 = (TextView) findViewById(R.id.tv2);
